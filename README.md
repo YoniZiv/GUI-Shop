d0622106f1e80f07c0d93b62dae0ca689a3e31b6
YoniZiv <yziv95@gmail.com>
2022-11-18 02:22:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GUI-Shop
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-a746wc)
---
+++ src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { Component, VERSION } from '@angular/core';
+import { data } from './GUIShop';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+  data = {};
+  shopList = {};
+  deletedItems = {};
+  onstructor(copier) {
+    this.copier = copier;
+  }
+  ngOnInit() {
+    this.data = data;
+    this.shopList = this.data['Shop - Shop List'];
+    console.log(this.shopList);
+  }
+
+  getKeys(item: any) {
+    return Object.keys(item);
+  }
+
+  deleteItem(key: string) {
+    this.deletedItems[key] = this.shopList[key];
+    delete this.shopList[key];
+  }
+
+  restoreItem(key: string) {
+    this.shopList[key] = this.deletedItems[key];
+    delete this.deletedItems[key];
+  }
+
+  copyShop() {
+    this.copier.copyText(JSON.stringify(this.shopList));
+    this.data['Shop - Shop List'] = this.shopList;
+    const shopKeys = this.getKeys(this.shopList);
+    const categories = this.data['Shop - Shop Categories'];
+    const newCats = Object.keys(categories).reduce((acc, next) => {
+        const currCat = categories[next];
+        const filteredCatItems = currCat.Items.filter((x) => shopKeys.indexOf(x) !== -1);
+        currCat.Items = filteredCatItems;
+        acc[next] = currCat;
+        return acc;
+    }, {});
+    this.data['Shop - Shop Categories'] = newCats;
+    console.log('new', this.data['Shop - Shop Categories']);
+}
+}
